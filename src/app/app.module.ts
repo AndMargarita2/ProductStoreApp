bd5982c5224b11db8df89dcfe177fa97013c90be
AndMargarita2 <cameron.malfoy@gmail.com>
2025-03-28 15:52:13 -0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CartComponent } from './views/cart/cart.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProductsComponent } from './views/products/products.component';
+import { ProductDetailsComponent } from './views/product-details/product-details.component';
+import { AboutComponent } from './views/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CartComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductDetailsComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
